e5b9ff0db570bd22d727f0abb0568e007392724d
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2025-02-12 20:54:04 +0500

    zero buff

--- src/specificationsIcebox/iceboxOneAndHalf.js
+++
@@ -1,8 +0,0 @@
-const iceboxOneAndHalf = [
-    { name: 'coldwell s800sd', height: '2000', width: '900', depth: '720', weight: '130', polki: '4/5 + дно' },
-    { name: 'inter 600', height: '2070', width: '990', depth: '680', weight: '156', polki: '4/5 + дно' },
-    { name: 'fvs 1000', height: '2030', width: '1030', depth: '690', weight: '185', polki: '4/5 + дно' },
-
-
-]
-export default iceboxOneAndHalf;
